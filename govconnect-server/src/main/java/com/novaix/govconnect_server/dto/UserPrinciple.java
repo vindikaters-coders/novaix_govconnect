9f533116779a3881b24d2346001e138944b1b6a0
vinuth <vinuthsriarampath@outlook.com>
2025-08-13 16:57:55 +0530

    feat: implement JWT authentication and CORS configuration

---
+++ govconnect-server/src/main/java/com/novaix/govconnect_server/dto/UserPrinciple.java
@@ -0,0 +1,30 @@
+package com.novaix.govconnect_server.dto;
+
+import com.novaix.govconnect_server.dao.UsersDao;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserPrinciple implements UserDetails {
+
+    private transient UsersDao usersDao;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return usersDao.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return usersDao.getEmail();
+    }
+}
---
+++ govconnect-server/src/main/java/com/novaix/govconnect_server/service/auth/impl/JwtService.java
@@ -0,0 +1,96 @@
+package com.novaix.govconnect_server.service.auth.impl;
+
+import com.novaix.govconnect_server.dto.UserPrinciple;
+import com.novaix.govconnect_server.exeption.custom.InternalServerErrorException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final String secretKey;
+
+    public JwtService(){
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGenerator.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        }catch (NoSuchAlgorithmException e){
+            throw new InternalServerErrorException("Error initializing JWT service: " + e.getMessage());
+        }
+    }
+
+    public String generateToken(Authentication authentication) {
+        UserPrinciple userPrincipal = (UserPrinciple) authentication.getPrincipal();
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("roles", userPrincipal.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList());
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userPrincipal.getUsername())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(getKey())
+                .compact();
+    }
+
+    private SecretKey getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String extractUsername(String token){
+        return extractClaim(token, Claims::getSubject);
+    }
+    private Date extractExpiration(String token){
+        return extractClaim(token,Claims::getExpiration);
+    }
+    private <T>T extractClaim(String token, Function<Claims,T> claimResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String username= extractUsername(token);
+        if(!username.equals(userDetails.getUsername())){
+            throw new JwtException("Username dose not match with the token subject!");
+        }
+        if (isTokenExpired(token)){
+            throw new JwtException("Token is Expired!");
+        }
+        if (!userDetails.isEnabled()){
+            throw new JwtException("User is Disabled!");
+        }
+        return (true);
+    }
+}
