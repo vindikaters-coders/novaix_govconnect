1205ee8ca26c86e8c1aa6cddd23980ce9827fc57
vinuth <vinuthsriarampath@outlook.com>
2025-08-14 20:36:09 +0530

    fix: remove unnecessary comment in DatabaseSeeder and suppress unused warnings

--- govconnect-server/src/main/java/com/novaix/govconnect_server/seeder/DatabaseSeeder.java
+++ govconnect-server/src/main/java/com/novaix/govconnect_server/seeder/DatabaseSeeder.java
@@ -19,13 +19,14 @@ import java.time.LocalDate;
 @Configuration
 @RequiredArgsConstructor
 @Profile("dev")
+@SuppressWarnings("unused")
 public class DatabaseSeeder {
 
     private final BCryptPasswordEncoder encoder;
     @Bean
     CommandLineRunner initDatabase(UserRepository userRepository) {
         return args -> {
-            if (userRepository.count() == 0) { // Avoid duplicate insertions
+            if (userRepository.count() == 0) {
                 userRepository.save(new UsersDao(null, "Amal", "Perera","0710000000","superadmin@email.com", encoder.encode("Password@123"),new Address("Colombo","Bambalapitiya","Western","No. 12 Galle Road"), LocalDate.parse("2004-09-18"), Gender.MALE,"111111111111", Role.ROLE_SUPERADMIN));
                 userRepository.save(new UsersDao(null, "Saman", "kumara","0711111111","admin@email.com", encoder.encode("Password@123"),new Address("Kandy","Peradeniya","Central","45 Temple Street"), LocalDate.parse("2004-09-18"), Gender.MALE,"222222222222", Role.ROLE_ADMIN));
                 userRepository.save(new UsersDao(null, "Nuwan", "Kulasekara","0712222222","user@email.com", encoder.encode("Password@123"),new Address("Galle","Unawatuna","Southern","23 Beach Road"), LocalDate.parse("2004-09-18"), Gender.FEMALE,"333333333333", Role.ROLE_USER));
