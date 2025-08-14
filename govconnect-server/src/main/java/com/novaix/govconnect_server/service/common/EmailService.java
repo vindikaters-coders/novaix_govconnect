dd1cbfe5c55fa15a0bf048520f7e68a8975d5df9
vinuth <vinuthsriarampath@outlook.com>
2025-08-14 22:29:40 +0530

    feat: implement email service for sending emails using email templates

---
+++ govconnect-server/src/main/java/com/novaix/govconnect_server/service/common/EmailService.java
@@ -0,0 +1,12 @@
+package com.novaix.govconnect_server.service.common;
+
+import com.novaix.govconnect_server.enums.Role;
+import jakarta.mail.MessagingException;
+
+import java.util.Map;
+
+public interface EmailService {
+    void sendEmail(String to, String subject, String templateName, Map<String, Object> templateVariables) throws MessagingException;
+
+    void SendRegistrationSuccessEmail(String email, String name, Role role);
+}
